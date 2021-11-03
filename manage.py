caef0471023fd3acdcecbdf3ead3ec84d9bc1e54
Arefeh <arefehahmadi902@gmail.com>
2021-11-03 11:35:17 -0700

    update manage.py - add stu command

--- manage.py
+++ manage.py
@@ -4,30 +4,35 @@ from courses import courses
 
 f = open('14001/2903.txt', "r")
 
-b = f.read()
+scrap = f.read()
 f.close()
-a = json.loads(b)
+data = json.loads(scrap)
 
 """
 use cmp to cmpare n courses
 and write the name of n courses
+
+use stu to get list of a student's courses
 
 """
 
 res = []
 if len(sys.argv) > 0:
 	if sys.argv[1].lower() == 'cmp':
-		c = a[courses[sys.argv[2]]]['students']
+		c = data[courses[sys.argv[2]]]['students']
 		for i in range(len(c)):
 			flag = 1
 			for x in range(3,len(sys.argv)):
-				if c[i] not in a[courses[sys.argv[x]]]['students']:
+				if c[i] not in data[courses[sys.argv[x]]]['students']:
 					flag = 0
 					break
-			if flag:
+			if flag: 
 				res.append(c[i])
-	if sys.argv[1].lower() == 'get_student':
-		print("will be implemented soon")
+	if sys.argv[1].lower() == 'stu':
+		name = [sys.argv[2], sys.argv[3]]
+		for i in data.keys():
+			if name in data[i]["students"]:
+				res.append(data[i]["title"])
 
 	print(len(res))
 	print(res)
